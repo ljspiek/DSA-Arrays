c2313b3a9b81e7f8cf26d8cc8769b3bf25faaa6a
Laura Spieker <ljspiek@gmail.com>
2020-04-18 17:51:14 -0500

    function maxSum

---
+++ maxSum.js
@@ -0,0 +1,20 @@
+function maxSum(arr) {
+    //keep track of current sum
+    let currentSum = 0
+    //keep track of maximum sum
+    let maxiSum = 0
+
+    for(let i = 0; i < arr.length; i++) {
+        let currentNum = arr[i]
+
+        currentSum = Math.max((currentSum + currentNum), 0)
+
+        maxiSum = Math.max(currentSum, maxiSum)
+    }
+
+    return maxiSum
+}
+
+console.log(maxSum([4,6,-3,5,-3,1]))
+
+//Math.max(a, b) returns the larger of the two values provided
